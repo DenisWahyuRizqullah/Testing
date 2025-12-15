af07350677da2448cd41ac5437f3580500fb765a
DenisWahyuRizqullah <deniswahyu26@gmail.com>
2025-12-15 14:28:32 +0700

    first commit testing

---
+++ C++/while.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=0;
+    do{
+        
+        cout<<"hello "<<+a<<endl;
+        
+        a++;
+        if(a==5) break;
+        
+        
+    }while(b<=10);
+
+    // firt commit testing 
+
+
+}
